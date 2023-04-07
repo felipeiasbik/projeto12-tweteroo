9a16249888d2b76b2d086213602646dad385dd95
Felipe Pires Iasbik <felipeiasbik@hotmail.com>
2023-04-07 10:34:32 -0300

    Conexão de servidor

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Olá!!!")
+});
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Running server on port ${PORT}`));
